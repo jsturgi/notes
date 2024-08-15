526901276eb145a5c4a6daa8e055305a53044bc5
jsturgi <jsturgi@iu.edu>
2024-08-14 20:34:48 -0400

    30 days of python day 2

---
+++ Python/30DaysOfPython/02_Built_In_Functions/variables.py
@@ -0,0 +1,47 @@
+# Day 2: 30 Days of python programming
+first_name = 'Jake'
+last_name = 'Sturgill'
+full_name = first_name + ' ' + last_name
+country = 'United States'
+city = 'Indianapolis'
+age = 26
+year = 2024
+is_married = False
+is_true = True
+is_light_on = True
+joe, bob, sally = 'Joe', 'Bob', 'Sally'
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(joe))
+print(type(bob))
+print(type(sally))
+print(len(first_name))
+print( "first_name: ", len(first_name),  " last_name: ", len(last_name))
+num_one = 5 
+num_two = 4
+total = num_one + num_two
+product = num_two * num_one
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+r = 30
+pi = 3.14
+area_of_circle = pi * r ** 2
+circum_of_circle = 2 * pi * r
+r = input('Enter the radius of the circle: ')
+r = int(r)
+area_of_circle = pi * r ** 2
+usr_name = input('Enter your name: ')
+usr_country = input('Enter your country: ')
+usr_age = input('Enter your age: ')
+
+print(help('keywords'))
